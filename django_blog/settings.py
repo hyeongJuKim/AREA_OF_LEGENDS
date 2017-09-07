da4c3b0c45523fdbe077867c5f5d07b1891e9e1b
hj <gudwndhkd@nate.com>
2017-09-07 18:06:07 +0900

    기본 레이아웃 추가, 이미지 추가

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hpf*9=y_lkuk$$in&4or=x^zoyxq%ah3r^f2i_eqhw6cq^@56='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'blog', 'static'),
+]
